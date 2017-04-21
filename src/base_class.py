a3a82ee1b9841671be0fe85e6042a3d054ba2cef
wqxl309 <wqxl309@hotmail.com>
2017-04-21 11:23:20 +0800

    basically working, need more to details

--- src/base_class.py
+++ src/base_class.py
@@ -1,23 +1,29 @@
-import threading
 import time
 import datetime as dt
 import numpy as np
 import pandas as pd
 from WindPy import w
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+import matplotlib.ticker as mtick
+import matplotlib as mpl
+
 
 
 class portfolio:
 
-    FLUSH_POOL = 5
+    FLUSH_POOL = 1
     FLUSH_CWSTAT=1
     UNDL_POOL={'total':set([])}   # 数据结构 dict： {'total' : 所有标的 set , 'pofname1': {'stocks': set , 'futures': set , ...} , 'pofname2': {...} , ... }
-    POOL_COLUMNS='rt_time,rt_last,rt_pct_chg'
+    POOL_COLUMNS='rt_time,rt_last'  #,rt_pct_chg'
     UNDL_POOL_INFO={}   # 存储以各个品种code为key的字典
+    PLOT_OBJ=[]   # 需要画图的对象
+    REGI_OBJ = [] # 所有已创建的对象
 
 
     @staticmethod
     def undlpool_callback(indata):
-        maxlen=3
+        maxlen=len(portfolio.POOL_COLUMNS.split(','))
         if indata.ErrorCode!=0:
             raise Exception('Error in callback with ErrorCode %d' %indata.ErrorCode)  # 实际使用时，为防止中断可改为log输出
         for dumi in range(len(indata.Codes)):
@@ -31,19 +37,55 @@ class portfolio:
 
     @classmethod
     def update_undlpool(cls):
+
         w.start()
-        underlyings=''
-        for undl in portfolio.UNDL_POOL['total']:
-            underlyings=underlyings.join(undl)
-        w.wsq(underlyings,portfolio.POOL_COLUMNS,func=portfolio.undlpool_callback)
+        tot=list(portfolio.UNDL_POOL['total'])
+        underlyings=''.join(tot)
+        underlyings=underlyings.replace("H","H,")
+        underlyings=underlyings.replace("Z","Z,")
+
+        w.wsq(underlyings,portfolio.POOL_COLUMNS,func=portfolio.undlpool_callback)   # 订阅POOL里面所有的undelryings
+
         today=dt.date.today()
-        start=dt.datetime(year=today.year, month=today.month,day=today.day,hour= 8,minute=0,second=0)
-        end=dt.datetime(year=today.year, month=today.month,day=today.day,hour= 15,minute=15,second=0)
+        start=dt.datetime(year=today.year, month=today.month,day=today.day,hour= 9,minute=30,second=0)
+        end=dt.datetime(year=today.year, month=today.month,day=today.day,hour= 15,minute=0,second=0)
+
+
+        # 画图配置
+        #mpl.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+        fig=plt.figure()
+        #plt.xticks(pd.date_range(start,end,freq='H'))#时间间隔
+
+        x = {}
+        y = {}
+        axes = {}
+
         while( end>= dt.datetime.now() >= start):
-            info="这个while循环主要是防止IDE在运行或者debug时，运行w.wsq()语句后就退出，从而导致行情推送过来后，回调函数无法运行！"
-            time.sleep(1)
-            print(portfolio.UNDL_POOL_INFO)
+            time.sleep(portfolio.FLUSH_CWSTAT)
+            for obj in portfolio.REGI_OBJ:
+                obj.update_object()
+
+            xkeys=x.keys()
+            count=0
+            for obj in portfolio.PLOT_OBJ:
+                if obj not in xkeys:
+                    x[obj]=[]
+                    y[obj]=[]
+                    ax=fig.add_subplot('12'+str(count+1))
+                    ax.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M'))#设置时间标签显示格式 '%Y-%m-%d %H:%M:%S'
+                    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.4f%%'))
+                    ax.set_title(obj.pofname)
+                    #ax.set_ylim(-0.02,0.02)
+                    axes[obj]=ax
+                y[obj].append((obj.addvalue['floated']+obj.addvalue['fixed'])/obj.pofvalue)
+                x[obj].append(dt.datetime.now())
+                axes[obj].set_xlim(x[obj][0],x[obj][-1])
+                axes[obj].plot(x[obj],y[obj],linewidth=1,color='r')
+                plt.pause(0.1)
+                count+=1
+
         print('temp: update finished')
+        ######   保存图像 ######
 
 
     @classmethod
@@ -66,79 +108,103 @@ class portfolio:
 
 
     @classmethod
-    def pop_pool(cls,pofname):
+    def pop_pool(cls,pofname,poplst):
         # 从 pool 中删除
         # 根据特定 portfolio pop
         poolkeys=portfolio.UNDL_POOL.keys()
         if pofname in poolkeys:
-            holdonly=portfolio.UNDL_POOL[pofname]  # 提取该组合特有的股票删除，不能删除 total 中与其他组合共有的股票
+            #holdonly = portfolio.UNDL_POOL[pofname]  # 提取该组合特有的股票删除，不能删除 total 中与其他组合共有的股票
+            holdonly = poplst
             for k in poolkeys:
                 if k not in ( 'total', pofname):
                     for undl in holdonly.keys():
                         holdonly[undl] -= portfolio.UNDL_POOL[k][undl]
             for und in holdonly.keys():      # 删除 total 中对应部分
                 portfolio.UNDL_POOL['total'] -= holdonly[und]
-            del portfolio.UNDL_POOL[pofname]  # 删除对应产品
+            del portfolio.UNDL_POOL[pofname]  # 删除对应产品, 可能仍有部分碎股会存在
 
 
     def __init__(self,pofname,pofvalue,hldlstdir,trdlstdir,cwstatusdir):
-        self.pofname=pofname
-        self.pofvalue=pofvalue                 # 产品前一日总资产
-        self.hldlstdir=hldlstdir               # 基于标的的字典 ex. {'stocks':dir1,futures:dir2}
-        self.trdlstdir=trdlstdir               # 基于标的的字典
-        self.cwstatusdir=cwstatusdir
-        self.lasttrdstat=self.get_trdstat()   # 创建对象的时间应该在 开始交易时间之前 （开盘前 或 9：35 之前）
-        self.noposition=not np.any(self.lasttrdstat[:,0])
-
-        self.addvalue={'fixed':0 ,'floated':0 }  # 若在当天出场则为fixed收益，否则为floated
-        self.holdlist={'T1':{},'T0':{}}     # holdlist数据结构 ： { 'T1':{}, 'T0':{}} , T+0/T+1 部分分别存储一个基于品种的、类似lst的dict
+        self.pofname = pofname
+        self.pofvalue = pofvalue                 # 产品前一日总资产
+        self.hldlstdir = hldlstdir               # 基于标的的字典 ex. {'stocks':dir1,futures:dir2}
+        self.trdlstdir = trdlstdir               # 基于标的的字典
+        self.cwstatusdir = cwstatusdir
+        self.lasttrdstat = self.get_trdstat()   # 创建对象的时间应该在 开始交易时间之前 （开盘前 或 9：35 之前）
+        self.noposition = not np.any(self.lasttrdstat[:,0])
+        self.addvalue = {'fixed':0 ,'floated':0 }  # 若在当天出场则为fixed收益，否则为floated
+        self.holdlist = {'T1':{},'T0':{}}     # holdlist数据结构 ： { 'T1':{}, 'T0':{}} , T+0/T+1 部分分别存储一个基于品种的、类似lst的dict
 
         if not self.noposition: # 交易开始前就有持仓的情况下，加入holdlist, 当天开始时无持仓则不必
-            self.holdlist['T1']=self.read_holdlist()
+            self.holdlist['T1'] = self.read_holdlist()
             if self.holdlist['T1']:
                 portfolio.add_pool(self.holdlist['T1'],self.pofname)
+                portfolio.PLOT_OBJ.append(self)
         else:
-            self.holdlist=None
+            self.holdlist = None
+        portfolio.REGI_OBJ.append(self)
 
 
     def get_trdstat(self):
         # 返回从cwstate.txt读取的内容，类型为np array
         with open(self.cwstatusdir,'r') as cwinfo:
-            temp=cwinfo.readlines()
-            contents_temp=[c.strip().split(',') for c in temp]
-            contents=[[int(c) for c in t] for t in contents_temp]
+            temp = cwinfo.readlines()
+            contents_temp = [c.strip().split(',') for c in temp]
+            contents = [[int(c) for c in t] for t in contents_temp]
         return np.array(contents)
 
 
     def flush_trdstat(self):
         # 根据持仓状态变更，更新 holdlist以及资产池，并更新 lasttrdstat
-        currtrdstat=self.get_trdstat()
-        statchg=currtrdstat[:,0]-self.lasttrdstat[:,0]
+        currtrdstat = self.get_trdstat()
+        statchg = currtrdstat[:,0]-self.lasttrdstat[:,0]
+        updtstat = False
+        statchg = 0
         if np.any(statchg):   # 持仓有变动
             if not np.any(currtrdstat[:,0]): # 从有持仓变为空仓
-                trdlst=self.read_trdlist()
-                self.update_holdlist(trdlst['out'],'T1')  # 当天只能卖出T+1的股票
-                self.noposition=True
-                portfolio.pop_pool(self.pofname)
+                trdlst = self.read_trdlist()
+                if trdlst['out']:
+                    self.update_holdlist(trdlst['out'],'T1')  # 当天只能卖出T+1的股票
+                    portfolio.pop_pool(self.pofname,trdlst['out'])
+                    self.noposition = True
+                    updtstat = True   # 只有在trdlist完成提取后才会更新 trdstat, 防止trdlist 更新较慢的情况
+                    statchg = -1
             elif not np.any(self.lasttrdstat[:,0]): # 从无持仓变为有持仓
-                trdlst=self.read_trdlist()
-                self.holdlist['T0']=trdlst['in']     # 当天买入的股票算作 T+0
-                self.noposition=False
-                portfolio.add_pool(self.holdlist['T0'],self.pofname)
+                trdlst = self.read_trdlist()
+                if trdlst['in']:
+                    self.holdlist['T0'] = trdlst['in']     # 当天买入的股票算作 T+0
+                    portfolio.add_pool(self.holdlist['T0'],self.pofname)
+                    self.noposition = False
+                    updtstat = True
+                    statchg = 1
             else:  # 有持仓，且持仓变动  # need more contents ,pause for now
                 trdlst=self.read_trdlist()
-                self.update_holdlist(trdlst['in'],'T0')
-                self.update_holdlist(trdlst['out'],'T1')
-                portfolio.add_pool(trdlst['in'],self.pofname)
-        self.lasttrdstat=currtrdstat
+                if trdlst['in'] and trdlst['out']:
+                    self.update_holdlist(trdlst['in'],'T0')
+                    self.update_holdlist(trdlst['out'],'T1')
+                    portfolio.add_pool(trdlst['in'],self.pofname)
+                    updtstat = True
+        if updtstat:
+            self.lasttrdstat=currtrdstat
+        return statchg
+
+
+    def read_trdlist(self):  # 返回 买入 卖出两个方向的单子
+        trdlist={}
+        trdlist['in']={}
+        trdlist['out']={}
+        ###################################################
+
+        ###################################################
+        return trdlist
 
 
     def read_holdlist(self):
         # 返回字典结构,字典的key是各个不同品种的标的，如stocks\futures\options etc
         holdlist={}
         for k in self.hldlstdir.keys():
-            holdlist[k]=pd.read_csv(self.hldlstdir[k],encoding='gb2312',names=['code','name','num','prc','val'])
-            holdlist[k].index=holdlist[k]['code'].tolist()
+            holdlist[k] = pd.read_csv(self.hldlstdir[k],encoding='gb2312',names=['code','name','num','prc','val'])
+            holdlist[k].index = holdlist[k]['code'].tolist()
         return holdlist
 
 
@@ -150,72 +216,71 @@ class portfolio:
                 codes = lst[k]['code']
                 num = lst[k]['num']
                 self.holdlist['T1'][k].loc[codes,'num'].values += num
-                self.addvalue['fixed']+=(self.holdlist['T1'][k].loc[codes,'prc'].values-lst[k]['prc'])*num
+                self.addvalue['fixed'] += (self.holdlist['T1'][k].loc[codes,'prc'].values-lst[k]['prc'])*num
         else:      # 只在当日入场时更新 T+0
             if len(self.holdlist['T0'])==0:
-                self.holdlist['T0']=lst
+                self.holdlist['T0'] = lst
             else:
                 for k in lst.keys():
                     temp=pd.concat([self.holdlist['T0'][k],lst[k]],ignore_index=True)   # type:pd.DataFrame
                     grouped=temp.groupby('code').sum()
-                    grouped['code']=grouped.index
-                    grouped['prc']=grouped['val'].values/grouped['num'].values
-                    self.holdlist['T0']=grouped
-
-
-    def read_trdlist(self):  # 返回 买入 卖出两个方向的单子
-        trdlist={}
-        trdlist['in']={}
-        trdlist['out']={}
-        pass
-
-    
-    def take_from_pool(self):
-        if self.noposition:
-            pass
+                    grouped['code'] = grouped.index
+                    grouped['prc'] = grouped['val'].values/grouped['num'].values
+                    self.holdlist['T0'] = grouped
 
     
     def update_addvalue(self):
         # 只计算尚未卖出的收益，即floated 收益
         # 当日卖出的部分属于 fixed 收益，在 update_holdlist 中计算
-        newinfo=pd.DataFrame(portfolio.UNDL_POOL_INFO,index=portfolio.UNDL_POOL_INFO.keys())
-        addval=0
+        newinfo = pd.DataFrame( portfolio.UNDL_POOL_INFO, index = portfolio.POOL_COLUMNS.split(',')  ).T
+        addval = 0
         for tp in self.holdlist.keys():
             holding=self.holdlist[tp]
             for k in holding.keys():
-                addval+=(newinfo[holding[k]['code'].values].values - holding[k]['prc']) * holding[k]['num'].values
-        self.addvalue['floated']=addval
-
-    
-    def run(self):
-        pass
+                holdingK=holding[k]
+                addval += np.sum( (newinfo.loc[holdingK['code'].tolist(),'rt_last'].values - holdingK['prc'].values) * holdingK['num'].values )
+        self.addvalue['floated'] = addval
 
 
-    def startplot(self):
-        pass
+    def startplot(self):   # 如需画图，则将该产品 对象 添加到 portfolio 类画图列表中
+        portfolio.PLOT_OBJ.append(self)
 
 
+    def stopplot(self):
+        portfolio.PLOT_OBJ.remove(self)
 
 
-class test:
-    a=0
+    def update_object(self):  # 定时扫描交易状态 trdlistdir 并更新 holdlist
+        time.sleep(portfolio.FLUSH_CWSTAT)
+        statchg = self.flush_trdstat()
+        if statchg == 1:
+            self.startplot()
+        elif statchg == -1:
+            self.stopplot()
+        self.update_addvalue()
 
-    def __init__(self,v):
-        test.a+=1
-        self.b=v
+
 
 
 if __name__=='__main__':
-    pofname='test'
-    pofvalue=1000
-    hldlstdir={'stocks' : '..\m_JQ1ICLong20170412.csv'}
-    trdlstdir=''
-    cwstatusdir=r'..\cwstate.txt'
+    pofname1='test1'
+    pofvalue1=820386
+    hldlstdir1={'stocks' : '..\BQ1ICLong20170421.csv'}
+    trdlstdir1=''
+    cwstatusdir1=r'..\cwstate1.txt'
 
-    t=portfolio(pofname,pofvalue,hldlstdir,trdlstdir,cwstatusdir)
+    t1=portfolio(pofname1,pofvalue1,hldlstdir1,trdlstdir1,cwstatusdir1)
+
+    pofname2='test2'
+    pofvalue2=820386
+    hldlstdir2={'stocks' : '..\BQ1ICLong20170421.csv'}
+    trdlstdir2=''
+    cwstatusdir2=r'..\cwstate2.txt'
+
+    t2=portfolio(pofname2,pofvalue2,hldlstdir2,trdlstdir2,cwstatusdir2)
 
     portfolio.update_undlpool()
-    # w.cancelRequest(0)
+    w.cancelRequest(0)
 
 
 
--- src/help_functions.py
+++ src/help_functions.py
@@ -6,3 +6,21 @@ def holdlist_format():
 
 def trdlist_format():
     pass
+
+
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+x=[]
+y=[]
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.set_xlim(0,100)
+for i in range(100):
+    x.append(i)
+    y.append(np.random.rand())
+    ax.plot(x,y,color='r')
+    #ax.plot(i,np.random.rand(),color='r')
+    plt.pause(0.1)
